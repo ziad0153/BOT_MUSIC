30e6096f741037ae453db2ac7fceac20e2550aff
Ziad Ahmed <160580962+ziad0153@users.noreply.github.com>
2024-08-29 00:42:09 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,45 @@
+const process = require('process');
+module.exports = {
+    app: {
+        token: process.env.DISCORD_TOKEN || 'xxx',
+        playing: 'by the Community ❤️',
+        global: true,
+        guild: process.env.GUILD_ID || 'xxx',
+        extraMessages: false,
+        loopMessage: false,
+        lang: 'en',
+        enableEmojis: false,
+    },
+
+    emojis:{
+        'back': '⏪',
+        'skip': '⏩',
+        'ResumePause': '⏯️',
+        'savetrack': '💾',
+        'volumeUp': '🔊',
+        'volumeDown': '🔉',
+        'loop': '🔁',
+    },
+
+    opt: {
+        DJ: {
+            enabled: false,
+            roleName: '',
+            commands: []
+        },
+        Translate_Timeout: 10000,
+        maxVol: 100,
+        spotifyBridge: true,
+        volume: 75,
+        leaveOnEmpty: true,
+        leaveOnEmptyCooldown: 30000,
+        leaveOnEnd: true,
+        leaveOnEndCooldown: 30000,
+        discordPlayer: {
+            ytdlOptions: {
+                quality: 'highestaudio',
+                highWaterMark: 1 << 25
+            }
+        }
+    }
+};
